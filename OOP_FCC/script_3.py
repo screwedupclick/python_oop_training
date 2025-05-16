a6f783e113f956853ad7b507629a8f206ecb9c1a
DuanrA <arnaudscrivo.dev@proton.me>
2025-05-16 14:40:01 +0200

    entraînement OOP Python

--- OOP_FCC/script_3.py
+++ OOP_FCC/script_3.py
@@ -1,5 +1,13 @@
 class User:
     def __init__(self, username, email, password):
         self.username = username
-        self.email = email
+        self._email = email
         self.password = password
+    
+
+        
+user1 = User("DarkVador", " Darkvador@gmail.com", "156")
+
+
+# En Python, _variable est "protégée" (convention : usage interne ou sous-classes), __variable est "privée" (name mangling pour limiter l'accès direct).
+# Ces mécanismes reposent sur des conventions et n'empêchent pas techniquement l'accès extérieur.
