6ef9a22661fb8c04f331b5dfb94eaee0f85a94ca
DuanrA <arnaudscrivo.dev@proton.me>
2025-05-19 14:54:01 +0200

    property & setter

--- OOP_FCC/script_5.py
+++ OOP_FCC/script_5.py
@@ -4,13 +4,19 @@ from datetime import datetime
 class User:
     def __init__(self, username, email, password):
         self.username = username
-        self.email = email
+        self._email = email
         self.password = password
         
     @property
     def email(self):
         print("Email accessed")
-        return self.email
+        return self._email
+    
+    @email.setter
+    def email(self, new_email):
+        if "@" in new_email:
+            self._email = new_email
     
 user1 = User("DarkVad0r", "DarkVador@gmail.com", "156")
+user1.email = "this is not an email"
 print(user1.email)
